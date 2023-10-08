2d21fb273743632a237fcfdb8ada292ce8568272
santiago <santiagodeandrade4@gmail.com>
2023-10-08 14:10:25 -0400

    Refactor de codigo anterior Adicion de mensaje de error para option invalida en el check_option

--- main.cpp
+++ main.cpp
@@ -8,25 +8,25 @@ using namespace std;
 #define BIGGEST_MENU_OPTION 4
 #define SMALLEST_MENU_OPTION 1
 #define MENU_OPTION_MAX_SIZE 20
+#define BAD_INPUT_MSG "Error, la opcion ingresada no es valida ... \n"
 
-int check_option_entered(std::string option){
-    int i = 0;
-    while ((int) option[i] != 0){
-        cout << option[i] << endl;
-        if ((57 < (int) option[i]) ||  ((int) (option[i]) < 48)){
-            cout << option[i] << endl;
-            cout << "Error, el usuario ha ingresado un caracter" << endl;
-            return 0;
+bool is_num(int ascii_char){
+    return (57 >= ascii_char) &&  (ascii_char >= 48);
+}
+
+bool option_is_valid(std::string option){
+    for (int i = 0; ((int) option[i]) != 0; i++){
+        if (!is_num((int) option[i])){
+            return false;
         }
-        i++;
     }
-    return 1;
+    return true;
 }
 
 void print_header(){
     system("clear");
-    cout << Y_SEPARATION << X_SEPARATION << "Santiago De Andrade, 31.065.103" << endl;
-    cout << X_SEPARATION << "Proyecto de Algoritmos y Estructuras de Datos" << endl;
+    cout << Y_SEPARATION << X_SEPARATION << "Santiago De Andrade, 31.065.103"               << endl;
+    cout << X_SEPARATION <<                 "Proyecto de Algoritmos y ascii_char de Datos"  << endl;
     cout << Y_SEPARATION << X_SEPARATION << "Pulsa enter para comenzar ... ";
     getchar();
 }
@@ -34,21 +34,30 @@ void print_header(){
 
 int print_menu(){
     std::string option;
-    do {
-        cout << Y_SEPARATION << X_SEPARATION << "1 - Agregar elementos"      << endl;
-        cout << X_SEPARATION                 << "2 - Modificar elementos"    << endl;
-        cout << X_SEPARATION                 << "3 - Buscar elementos"       << endl;
-        cout << X_SEPARATION                 << "4 - Eliminar elementos"     << endl;
-        cout << Y_SEPARATION << X_SEPARATION << "-> Ingresa una opcion : ";
+    bool failed = false;
+    while (1){
+        system("clear");
+        cout << Y_SEPARATION << X_SEPARATION << "1 ~ Agregar elementos"      << endl;
+        cout << X_SEPARATION                 << "2 ~ Modificar elementos"    << endl;
+        cout << X_SEPARATION                 << "3 ~ Buscar elementos"       << endl;
+        cout << X_SEPARATION                 << "4 ~ Eliminar elementos"     << endl;
+        if (failed)
+            cout << Y_SEPARATION << X_SEPARATION << BAD_INPUT_MSG << X_SEPARATION << "-> Ingresa una opcion : ";
+        else
+            cout << Y_SEPARATION << X_SEPARATION << "-> Ingresa una opcion : ";
         cin  >> option;
-    } while (check_option_entered(option) == 1);
-
+        if (!option_is_valid(option))
+            failed = true;
+        else
+            break;
+    };
     return 1;
 }
 
 
 int main(int argc, char **argv){
     int selected_option;
+    print_header();
     while (1){
         selected_option = print_menu();
     }
