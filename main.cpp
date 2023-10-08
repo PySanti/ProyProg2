066bb30e69aaabde9bbeadf45cf3bb0b4db3c6b2
santiago <santiagodeandrade4@gmail.com>
2023-10-08 12:18:59 -0400

    Inicio del proyecto Creacion de header

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void print_header(){
+    cout << "\n\n\t\tSantiago De Andrade, 31.065.103" << endl;
+    cout << "\t\tProyecto de Algoritmos y Estructuras de Datos" << endl;
+    cout << "\n\n\n\t\tPulsa enter para comenzar ..." << endl;
+    getchar();
+}
+
+
+int main(int argc, char **argv){
+    print_header();
+    return 0;
+}
