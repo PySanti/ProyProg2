3460260e7158fe8cf4fd745832aa6bdabad52a1d
santiago <santiagodeandrade4@gmail.com>
2023-11-11 08:03:13 -0400

    Adicion de structs : Article, Client, Seller

--- dependencies/structs.h
+++ dependencies/structs.h
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 typedef struct Node{
     int element;
     struct Node *next;
@@ -7,3 +11,23 @@ typedef struct List {
     Node *head;
 } List;
 
+typedef struct Article {
+    string code;
+    string name;
+    float price;
+    int count;
+} Article;
+
+typedef struct Client {
+    int id;
+    string name;
+    string direction;
+    string phone_number;
+} Client;
+
+typedef struct Seller {
+    int id;
+    string name;
+    string entry_date;
+    float comission;
+} Seller;
