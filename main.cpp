974560cb766a4e39aba499d7e5717f1beb2b950c
santiago <santiagodeandrade4@gmail.com>
2023-11-11 16:10:22 -0400

    Modificacion de menus principales del proyecto

--- main.cpp
+++ main.cpp
@@ -13,76 +13,89 @@ string options[] = {
     "Vendedores",
     "Comisiones por ventas",
     "Bonos",
-    "Facturas"
+    "Facturas",
+    "Salir"
 };
 string article_options[] = {
     "Agregar articulo",
     "Modificar articulo",
     "Eliminar articulo",
-    "Buscar articulo"
+    "Buscar articulo",
+    "Volver"
 };
 string stock_options[] = {
     "Agregar inventario",
     "Modificar inventario",
     "Eliminar inventario",
-    "Buscar inventario"
+    "Buscar inventario",
+    "Volver"
 };
 string client_options[] = {
     "Agregar cliente",
     "Modificar cliente",
     "Eliminar cliente",
-    "Buscar cliente"
+    "Buscar cliente",
+    "Volver"
 };
 string discount_options[] = {
     "-",
     "-",
     "-",
+    "Volver"
 };
 string seller_options[] = {
     "Agregar vendedor",
     "Modificar vendedor",
     "Eliminar vendedor",
-    "Buscar vendedor"
+    "Buscar vendedor",
+    "Volver"
 };
 string comission_options[] = {
     "-",
+    "Volver",
 };
 string bones_options[] = {
     "-",
+    "Volver"
 };
 
 
 
 
 int main(int argc, char **argv){
-    int selected_option = print_menu(options, 8, 1, "Menu Inicial");
-    switch (selected_option)
-    {
-        case 1:
-            selected_option = print_menu(article_options, 4, 1, "Menu de Artículos");
-            break;
-        case 2:
-
-            selected_option = print_menu(stock_options, 4, 1, "Menu de Inventario");
-            break;
-        case 3:
-
-            selected_option = print_menu(client_options, 4, 1, "Menu de Clientes");
-            break;
-        case 4:
-
-            selected_option = print_menu(discount_options, 3, 1, "Menu de Descuentos");
-            break;
-        case 5:
-            selected_option = print_menu(seller_options, 4, 1, "Menu de Vendedores");
-            break;
-        case 6:
-
-            selected_option = print_menu(comission_options, 1, 1, "Menu de comisiones");
-            break;
-        case 7:
-            selected_option = print_menu(bones_options, 1, 1, "Menu de Bonos");
-            break;
-    } (selected_option);
+    bool exit = false;
+    while (!exit){
+        int selected_option = print_menu(options, 9, 1, "Menu Inicial");
+        switch (selected_option)
+        {
+            case 1:
+                selected_option = print_menu(article_options, 4, 1, "Menu de Artículos");
+                break;
+            case 2:
+                selected_option = print_menu(stock_options, 4, 1, "Menu de Inventario");
+                break;
+            case 3:
+                selected_option = print_menu(client_options, 4, 1, "Menu de Clientes");
+                break;
+            case 4:
+                selected_option = print_menu(discount_options, 3, 1, "Menu de Descuentos");
+                break;
+            case 5:
+                selected_option = print_menu(seller_options, 4, 1, "Menu de Vendedores");
+                break;
+            case 6:
+                selected_option = print_menu(comission_options, 1, 1, "Menu de comisiones");
+                break;
+            case 7:
+                selected_option = print_menu(bones_options, 1, 1, "Menu de Bonos");
+                break;
+            case 8:
+                selected_option = print_menu(bones_options, 1, 1, "Menu de facturas");
+                break;
+            case 9:
+                exit = true;
+                break;
+        } (selected_option);
+    }
     return 0;
 }
