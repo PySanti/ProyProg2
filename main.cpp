8c3e34452cd791be32193416ebf8db8c7164a6f0
santiago <santiagodeandrade4@gmail.com>
2023-11-11 12:14:02 -0400

    Refactor del main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,51 @@
 #include "./dependencies/menus.h"
 using namespace std;
 
+string options[] = {
+    "Artículos",
+    "Inventario",
+    "Clientes",
+    "Descuentos",
+    "Vendedores",
+    "Comisiones por ventas",
+    "Bonos",
+    "Facturas"
+};
+string article_options[] = {
+    "Agregar articulo",
+    "Modificar articulo",
+    "Eliminar articulo",
+    "Buscar articulo"
+};
+string stock_options[] = {
+    "Agregar inventario",
+    "Modificar inventario",
+    "Eliminar inventario",
+    "Buscar inventario"
+};
+string client_options[] = {
+    "Agregar cliente",
+    "Modificar cliente",
+    "Eliminar cliente",
+    "Buscar cliente"
+};
+string discount_options[] = {
+    "-",
+    "-",
+    "-",
+};
+string seller_options[] = {
+    "Agregar vendedor",
+    "Modificar vendedor",
+    "Eliminar vendedor",
+    "Buscar vendedor"
+};
+string comission_options[] = {
+    "-",
+};
+string bones_options[] = {
+    "-",
+};
 
 
 List *create_list(){
@@ -52,73 +97,32 @@ void append_element(List *main_list, int element){
 
 
 int main(int argc, char **argv){
-    string options[] = {
-        "Artículos",
-        "Inventario",
-        "Clientes",
-        "Descuentos",
-        "Vendedores",
-        "Comisiones por ventas",
-        "Bonos",
-        "Facturas"
-    };
     int selected_option = print_menu(options, 8, 1, "Menu Inicial");
     switch (selected_option)
     {
         case 1:
-            string article_options[] = {
-                "Agregar articulo",
-                "Modificar articulo",
-                "Eliminar articulo",
-                "Buscar articulo"
-            };
             selected_option = print_menu(article_options, 4, 1, "Menu de Artículos");
             break;
         case 2:
-            string stock_options[] = {
-                "Agregar inventario",
-                "Modificar inventario",
-                "Eliminar inventario",
-                "Buscar inventario"
-            };
+
             selected_option = print_menu(stock_options, 4, 1, "Menu de Inventario");
             break;
         case 3:
-            string client_options[] = {
-                "Agregar cliente",
-                "Modificar cliente",
-                "Eliminar cliente",
-                "Buscar cliente"
-            };
+
             selected_option = print_menu(client_options, 4, 1, "Menu de Clientes");
             break;
         case 4:
-            string discount_options[] = {
-                "-",
-                "-",
-                "-",
-            };
+
             selected_option = print_menu(discount_options, 3, 1, "Menu de Descuentos");
             break;
         case 5:
-            string seller_options[] = {
-                "Agregar vendedor",
-                "Modificar vendedor",
-                "Eliminar vendedor",
-                "Buscar vendedor"
-            };
             selected_option = print_menu(seller_options, 4, 1, "Menu de Vendedores");
             break;
         case 6:
-            string comission_options[] = {
-                "-",
-            };
+
             selected_option = print_menu(comission_options, 1, 1, "Menu de comisiones");
             break;
         case 7:
-            string bones_options[] = {
-                "-",
-            };
             selected_option = print_menu(bones_options, 1, 1, "Menu de Bonos");
             break;
     } (selected_option);
