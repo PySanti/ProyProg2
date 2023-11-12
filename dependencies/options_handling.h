9b310ee789324a212c7bd6a4fed28a3b46d4b211
santiago <santiagodeandrade4@gmail.com>
2023-11-11 20:06:55 -0400

    Creacion de funciones para manejar opciones selecionadas

--- dependencies/options_handling.h
+++ dependencies/options_handling.h
@@ -22,18 +22,60 @@ void handle_article_option(int selected_option, string options_list[]){
     return;
 }
 
-void handle_article_option(int selected_option, string options_list[]){
+void handle_stock_option(int selected_option, string options_list[]){
+    return;
+}
+
+
+
+void handle_client_option(int selected_option, string options_list[]){
     string stringed_selected_option = to_lower(options_list[selected_option-1]);
     if (back_option_selected(stringed_selected_option))
         return;
     if (stringed_selected_option.find("agregar") != string::npos){
-        cout << "Agregar elemento a la lista de articulos" << endl;
+        cout << "Agregar cliente" << endl;
     } else if (stringed_selected_option.find("buscar") != string::npos){
-        cout << "Buscar elemento a la lista de articulos" << endl;
+        cout << "Buscar cliente" << endl;
     } else if (stringed_selected_option.find("eliminar") != string::npos){
-        cout << "Eliminar elemento a la lista de articulos" << endl;
+        cout << "Eliminar eliminar cliente" << endl;
     } else if (stringed_selected_option.find("modificar") != string::npos){
-        cout << "Modificar elemento a la lista de articulos" << endl;
+        cout << "Modificar cliente " << endl;
     }
+    return;
+}
+
+
+
+void handle_seller_option(int selected_option, string options_list[]){
+    string stringed_selected_option = to_lower(options_list[selected_option-1]);
+    if (back_option_selected(stringed_selected_option))
+        return;
+    if (stringed_selected_option.find("agregar") != string::npos){
+        cout << "Agregar vendedor" << endl;
+    } else if (stringed_selected_option.find("buscar") != string::npos){
+        cout << "Buscar vendedor" << endl;
+    } else if (stringed_selected_option.find("eliminar") != string::npos){
+        cout << "Eliminar vendedor" << endl;
+    } else if (stringed_selected_option.find("modificar") != string::npos){
+        cout << "Modificar vendedor " << endl;
+    }
+    return;
+}
+
+
+
+void handle_comission_option(int selected_option, string options_list[]){
+    return;
+}
+
+void handle_bones_option(int selected_option, string options_list[]){
+    return;
+}
+
+void handle_invoice_option(int selected_option, string options_list[]){
+    return;
+}
+
+void handle_discount_option(int selected_option, string options_list[]){
     return;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "./dependencies/options_handling.h"
 using namespace std;
 
-string options[] = {
+string main_options[] = {
     "Artículos",
     "Inventario",
     "Clientes",
@@ -71,34 +71,40 @@ string invoice_options[] = {
 int main(int argc, char **argv){
     bool exit = false;
     while (!exit){
-        int selected_option = print_menu(options, sizeof(options) / sizeof(options[0]), "Menu Inicial");
+        int selected_option = print_menu(main_options, sizeof(main_options) / sizeof(main_options[0]), "Menu Inicial");
         switch (selected_option)
         {
             case 1:
                 selected_option = print_menu(article_options, sizeof(article_options) / sizeof(article_options[0]), "Menu de Artículos");
                 handle_article_option(selected_option, article_options);
-                return 0;
                 break;
             case 2:
                 selected_option = print_menu(stock_options, sizeof(stock_options) / sizeof(stock_options[0]), "Menu de Inventario");
+                handle_stock_option(selected_option, article_options);
                 break;
             case 3:
                 selected_option = print_menu(client_options,sizeof(client_options) / sizeof(client_options[0]) , "Menu de Clientes");
+                handle_client_option(selected_option, client_options);
                 break;
             case 4:
                 selected_option = print_menu(discount_options,sizeof(discount_options) / sizeof(discount_options[0]), "Menu de Descuentos");
+                handle_discount_option(selected_option, client_options);
                 break;
             case 5:
                 selected_option = print_menu(seller_options,sizeof(seller_options) / sizeof(seller_options[0]) , "Menu de Vendedores");
+                handle_seller_option(selected_option, client_options);
                 break;
             case 6:
                 selected_option = print_menu(comission_options, sizeof(comission_options) / sizeof(comission_options[0]), "Menu de comisiones");
+                handle_comission_option(selected_option, client_options);
                 break;
             case 7:
                 selected_option = print_menu(bones_options, sizeof(bones_options) / sizeof(bones_options[0]), "Menu de Bonos");
+                handle_bones_option(selected_option, client_options);
                 break;
             case 8:
                 selected_option = print_menu(invoice_options, sizeof(invoice_options) / sizeof(invoice_options[0]), "Menu de facturas");
+                handle_invoice_option(selected_option, client_options);
                 break;
             case 9:
                 exit = true;
