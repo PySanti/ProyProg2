50855deb2a413f77a6bc2b3cdd13d998371cf992
santiago <santiagodeandrade4@gmail.com>
2023-11-25 09:30:05 -0400

    creacion de la funcion success_screen

--- dependencies/utils.h
+++ dependencies/utils.h
@@ -129,3 +129,11 @@ map<string, string> validate_form(map<string, string> patterns_dict){
     }
     return patterns_dict;
 }
+
+void success_screen(string msg){
+    system("clear");
+    cout << "\n\n\n\n\n\n\t\t\t\t" << uppercase(msg);
+    cout << "\n\n\n";
+    cout << "\t\t\t Pulse enter para continuar ... ";
+    getchar();
+}
